a3478f68700ba0705a1924bdadae7edd2a6e79d9
Adam Faris <medlem@faris.se>
2026-01-08 13:38:21 +0100

    Update Footer to use version from package.json and update VERSION file to 1.3.0

--- frontend/src/version.js
+++ frontend/src/version.js
@@ -1,3 +1,3 @@
 // Version information - auto-generated from package.json
 // This file is updated automatically during build
-export const FRONTEND_VERSION = '1.0.0';
+export const FRONTEND_VERSION = '1.3.0';
